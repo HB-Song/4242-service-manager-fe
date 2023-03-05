f9262c7bb97f270e13f5ea8a9ebf5bf016d99d60
송효범 <hyobum.song@spiceware.io>
2023-03-05 20:23:19 +0900

    카카오 SDK 연동
    카카오 SDK를 사용할 수 있게 SDK 연동 코드 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import { Configure } from '../constant/configure';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const { kakaoIntegryValue, applicationKey } = Configure;
+
+  const initKakao = () => {
+    window.Kakao.init(applicationKey);
+    console.log('Check Kakao Initialized : ', window.Kakao.isInitialized());
+  };
+  return (
+    <>
+      <Component {...pageProps} />
+      <Script
+        src="https://t1.kakaocdn.net/kakao_js_sdk/2.1.0/kakao.min.js"
+        integrity={kakaoIntegryValue}
+        onLoad={initKakao}
+        crossOrigin="anonymous"
+      ></Script>
+    </>
+  );
 }
