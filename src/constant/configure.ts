efe52a18612ee0b0617ca5a815a67aa79987d5ca
송효범 <hyobum.song@spiceware.io>
2023-03-05 20:22:19 +0900

    서버 설정값 파일 추가
    엑세스 키 등 보안이 필요한 값을 저장하기 위한 파일 추가

---
+++ src/constant/configure.ts
@@ -0,0 +1,4 @@
+export const Configure = {
+  kakaoIntegryValue: 'sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx',
+  applicationKey: '3a5678926c635a097b9fac8275b7e838',
+};
