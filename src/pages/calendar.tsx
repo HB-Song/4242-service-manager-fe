de6d47c0634e78d9155f6a355714ff075cdd93c6
송효범 <hyobum.song@spiceware.io>
2023-03-08 14:33:26 +0900

    템플릿 추가
    캘린더 구현을 위한 페이지 생성

---
+++ src/pages/calendar.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+export default function Calendar() {
+  return (
+    <>
+      <Head>
+        <title>4242 Calendar</title>
+        <meta name="description" content="벙 운동을 확인해 보아요!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>hello calendar!</div>
+      </main>
+    </>
+  );
+}
