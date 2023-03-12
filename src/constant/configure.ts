461e273aae34cae12e6923001624e17371117346
송효범 <hyobum.song@spiceware.io>
2023-03-12 18:56:51 +0900

    git ignore 추가
    설정 파일이 원격에 올라가면 탈취위험이 있기 때문에 git ignore 등록

--- src/constant/configure.ts
+++
@@ -1,6 +0,0 @@
-export const Configure = {
-  kakaoIntegryValue: 'sha384-dpu02ieKC6NUeKFoGMOKz6102CLEWi9+5RQjWSV0ikYSFFd8M3Wp2reIcquJOemx',
-  applicationKey: '3a5678926c635a097b9fac8275b7e838',
-  kakaoApiKey: 'c5b5080c15c74d35b003ac8ad3269def',
-  serverAddress: 'http://localhost:3000',
-};
